877afc343a617b2a640479cc6d9ae0aec89c079b
DrBlake123123 <107320645+DrBlake123123@users.noreply.github.com>
2022-06-12 00:49:04 +0530

    Add files via upload

---
+++ storytimeproject.js
@@ -0,0 +1,24 @@
+
+
+let greetings= "Great king of Asperg, Welcomes you";
+let nameofcharacter= 'Elias';
+let aka= " the god of peace";
+let armyGender= 'men of valor';
+
+
+let ageofcharacter= 43;
+let noOfsoldiers= 10000;
+let year= 1567;
+
+let supplies= ['shelter','water','food']
+
+let line= true;
+
+console.log(greetings + "!" + " My name is " + nameofcharacter + ".")
+console.log("Also known as," + aka + ".")
+console.log("The year right now is " + year + ",")
+console.log("making me " + ageofcharacter + " years old" + ".")
+console.log("My army is about " + noOfsoldiers + " soldiers, they are very brave and are " + armyGender + ".")
+console.log("We are short on " + supplies[2] + ", that my human friends said is very much needed for their survival")
+console.log("Which is " + line + " as they need for energy and to stay alive")
+console.log("I am preparing for a war against my rival, my nemesis, the king of Troy")
