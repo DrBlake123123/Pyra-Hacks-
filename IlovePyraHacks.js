1d5740a5e8b75bb8e4c1ec2b2fed67c9d22b53d7
DrBlake123123 <107320645+DrBlake123123@users.noreply.github.com>
2022-06-12 14:21:16 +0530

    Add files via upload

---
+++ IlovePyraHacks.js
@@ -0,0 +1,27 @@
+const sentence = ["I", "love", "Pyra Hacks","!"];
+ 
+let line = "";
+
+
+function formsentence(arr) {
+  
+  for (let counter = 0; counter < arr.length; counter++) {
+
+
+    if (counter === arr.length - 1) {
+      line = line + arr[counter];
+    }
+
+    else {
+    
+      line = line + arr[counter] + " ";
+      
+    }
+
+  }
+
+
+  return line;
+}
+
+console.log(formsentence(sentence));
